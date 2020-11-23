8b40f89b74c6df3b144f0a10f85a2ea7d41d81f2
rhysrawnson2101 <Rhysrawnson@gmail.com>
2020-11-23 15:22:42 +0000

    speeding ticket >70

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,8 @@
 def main():
     #write your code below this line
-
+    speed = int(input('please input your speed:'))
+    if speed >70:
+     print('speeding ticket')
+    
 if __name__ == '__main__':
     main()
